85b983e675011a7e66d7fbcebc44ce227cc75f5f
Werricsson <werricsson.santos@hotmail.com>
2025-08-17 18:49:32 -0300

    refactor(RootLayout): Corrigindo layout

--- src/components/RootLayout/index.tsx
+++ src/components/RootLayout/index.tsx
@@ -1,16 +1,24 @@
 import { Outlet } from 'react-router-dom';
+import { Box, Toolbar } from '@mui/material';
 import { Header } from '../Header';
-import { HomeContainer } from './styles';
 
 export function RootLayout() {
   return (
-    <>
-        <Header />
-        <HomeContainer>
-          <main>
-            <Outlet />  
-          </main>
-        </HomeContainer>
-    </>
+    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <Header />
+      
+      <Toolbar />
+
+      <Box 
+        component="main" 
+        sx={{ 
+          flexGrow: 1,
+          display: 'flex',
+          flexDirection: 'column'
+        }}
+      > 
+        <Outlet />
+      </Box>
+    </Box>
   );
 }
