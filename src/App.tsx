736cd73562791e9c57c8b8580fc282c0c2e92743
Werricsson <werricsson.santos@hotmail.com>
2025-08-17 18:53:31 -0300

    feat(layout): Implementa a nova estrutura de layout principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import { CssBaseline, GlobalStyles, ThemeProvider } from "@mui/material"
+import { CssBaseline, ThemeProvider } from "@mui/material"
+import { HeaderProvider } from './contexts/HeaderContext';
 import { darkTheme } from "./styles/theme";
-import { globalStyles } from "./styles/globalStyles";
 import { AppRouter } from "./router";
 
 export default function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-
-      <GlobalStyles styles={globalStyles} />
-      
-      <AppRouter />
+      <HeaderProvider>
+        <AppRouter />
+      </HeaderProvider>
     </ThemeProvider>
   )
 }
