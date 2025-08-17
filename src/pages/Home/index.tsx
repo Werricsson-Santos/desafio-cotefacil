c62c66977ad77ea97e002f5ff94ed0ade6ad9257
Werricsson <werricsson.santos@hotmail.com>
2025-08-17 18:45:16 -0300

    refactor(home): Adequando o visual da página inicial

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,26 @@
-import * as S from './styles';
+import { Box } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Header, Title, Subtitle} from './styles';
 
 export const Home: React.FC = () => {
   return (
-    <>
-      <S.Header>
-        <S.Title>Bem-vindo ao Portfólio</S.Title>
-        <S.Subtitle>
-          Selecione uma das aplicações no menu acima para começar a explorar.
-        </S.Subtitle>
-      </S.Header>
-    </>
+    <Box
+      sx={{
+        flexGrow: 1,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        p: 2,
+      }}
+    >
+      <Header>
+        <Title>Bem-vindo ao Portfólio</Title>
+        <Subtitle>
+          Selecione uma das aplicações no menu acima <MenuIcon sx={{ mx: 0.75, color: 'success.main', fontSize: '1.25rem', verticalAlign: 'middle'}} /> para começar a explorar.
+        </Subtitle>
+      </Header>
+    </Box>
   );
 };
